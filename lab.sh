b63a1b7014dcf3a4ba6e44875c376c1134d63f37
Jorge Andrade <jandrade@i-t-m.com>
2024-05-27 17:20:39 -0600

    Reformat

--- lab.sh
+++ lab.sh
@@ -35,7 +35,7 @@ hostname -f
 grep $(hostname -f) /etc/hosts && echo "FQDN [OK]" || echo "FQDN [FAILED]" 
 echo "IdM Instalacion"
 kinit admin && echo "Auth [OK]" || echo "Auth [FAILED]" 
-ipa server-find|grep $(hostname -f) && echo "Server [OK]" || echo "Server [FAILED]"
+ipa server-find|grep $(hostname -f) && echo "Server [OK]" || echo "Server [FAILED]" ; exit 1
 ipa server-show $(hostname -f)
 echo "IdM Domains"
 ipa dnszone-find
